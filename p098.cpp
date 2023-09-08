7310389d43ee01746348a046b871fe9fec542cb8
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-09-08 12:59:12 +0900

    P.98 経路復元

--- p098.cpp
+++ p098.cpp
@@ -4,38 +4,52 @@
 using namespace std;
 using namespace atcoder;
 
-// dp[k][i][j] k: k個のノードを見る, i: 始点, j: 終点, value: 最短距離
-int dp[101][101][101];
+vector<pair<int, int>> path[1001];
+int dist[1001];
+// 1つ前のノードを格納
+int pre_nodes[1001];
 
-// P.98 ワーシャルフロイド法
+// P.98 経路復元
 int main(int argc, char* argv[]) {
   bool is_debug = string(argv[0]) == "./test.out";
 
-  fill((int*) dp, (int*) (dp + 101), 1e9);
-
   int N, M;
   cin >> N >> M;
-  for (int i = 0; i < N; i++) dp[0][i][i] = 0;
+
   for (int i = 0; i < M; i++) {
     int u, v, c;
     cin >> u >> v >> c;
     u--; v--;
-    dp[0][u][v] = min(dp[0][u][v], c);
-    dp[0][v][u] = min(dp[0][v][u], c);
+    path[u].push_back({ c, v });
+    path[v].push_back({ c, u });
   }
 
-  for (int k = 1; k <= N; k++) for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) {
-    dp[k][i][j] = min(dp[k - 1][i][j], dp[k - 1][i][k - 1] + dp[k - 1][k - 1][j]);
-  }
+  fill((int*) dist, (int*) (dist + 1001), 1e9);
+  dist[0] = 0;
+  fill((int*) pre_nodes, (int*) (pre_nodes + 1001), -1);
 
-  for (int k = 0; k <= N; k++) {
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        cout << dp[k][i][j] << ' ';
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
+  que.push({ 0, 0 });
+
+  while (!que.empty()) {
+    int u = que.top().second;
+    que.pop();
+    for (pair<int, int> p : path[u]) {
+      int v = p.second, c = p.first;
+      if (dist[v] > c + dist[u]) {
+        dist[v] = dist[u] + c;
+        que.push({ dist[v], v });
+        pre_nodes[v] = u;
       }
-      cout << endl;
     }
-    cout << endl << endl;
   }
-  cout << dp[N][0][N - 1] << endl;
+
+  for (int i = 0; i < N; i++) cout << dist[i] << ' ';
+  cout << endl;
+  int i = N - 1;
+  do {
+    cout << i << ' ';
+    i = pre_nodes[i];
+  } while(i != -1);
+  cout << endl;
 }
