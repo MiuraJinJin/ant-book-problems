c2c1f3285c65988edccf12d2ebd3ccb657926b84
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-09-09 11:33:24 +0900

    P.100 最小全域木問題２(クラスカル法)

--- p100.cpp
+++ p100.cpp
@@ -4,54 +4,31 @@
 using namespace std;
 using namespace atcoder;
 
-int dist[1001];
-vector<pair<int, int>> path[1001];
-
-// P.100 最小全域木(MST, Minimum Spanning Tree, 辺の和が最小となり全てのノードに行き来可能な連結成分)
-// プリム法(ダイクストラの要領で採用する辺を決めていく)
+// P.101 最小全域木(MST, Minimum Spanning Tree, 辺の和が最小となり全てのノードに行き来可能な連結成分)
+// クラスカル法(コストの低い順に辺を繋げていき既出の場合無視する)
 int main(int argc, char* argv[]) {
   bool is_debug = string(argv[0]) == "./test.out";
 
   int N, M;
   cin >> N >> M;
 
+  priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> path;
   for (int i = 0; i < M; i++) {
     int u, v, c;
     cin >> u >> v >> c;
-    u--; v--;
-    path[u].push_back({ v, c });
-    path[v].push_back({ u, c });
+    path.push({ c, --u , --v });
   }
 
-  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
-  que.push({ 0, 0 });
-
-  fill((int*) dist, (int*) (dist + 1001), 1e9);
-  dist[0] = 0;
-
-  set<int> checked;
-
+  dsu uf(N);
   int ans = 0;
 
-  while (!que.empty()) {
-    int u = que.top().second, c = que.top().first;
-    que.pop();
-    if (checked.contains(u)) continue;
-    checked.insert(u);
+  while (!path.empty()) {
+    tuple<int, int, int> tpl = path.top();
+    path.pop();
+    auto [c, u, v] = tpl;
+    if (uf.same(u, v)) continue;
+    uf.merge(u, v);
     ans += c;
-
-    for (pair<int, int> p : path[u]) {
-      int v = p.first, c = p.second;
-      if (dist[v] > dist[u] + c) {
-        dist[v] = dist[u] + c;
-        que.push({ c, v });
-      }
-    }
-  }
-
-  if (is_debug) {
-    for (int i = 0; i < N; i++) cout << dist[i] << ' ';
-    cout << endl;
   }
 
   cout << ans << endl;
