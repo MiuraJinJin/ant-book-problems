c959c9f95cd606dc3e1cc115fa6cb4bb0f1f5ed7
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-09-18 01:22:13 +0900

    P.123 Millionaire

--- p123.cpp
+++ p123.cpp
@@ -6,7 +6,6 @@ using namespace atcoder;
 typedef long long ll;
 bool is_debug;
 
-
 int main(int argc, char* argv[]) {
   is_debug = string(argv[0]) == "./test.out";
 
@@ -18,28 +17,24 @@ int main(int argc, char* argv[]) {
   cin >> X;
 
   map<ll, double> mp;
+
   const int OK_VALUE = 1000000;
-  mp[-1e18] = 0.0;
+  mp[0] = 0.0;
   mp[OK_VALUE] = 1.0;
   for (int i = 1; i <= M; i++) {
     map<ll, double> new_mp;
-    new_mp[-1e18] = 0.0;
+    new_mp[0] = 0.0;
     new_mp[OK_VALUE] = 1.0;
     int unit = OK_VALUE / (1 << i);
     for (int j = 1; j < 1 << i; j++) {
-      double p1 = mp.lower_bound(j * unit + unit)->second * P;
-      double p2 = mp.lower_bound(j * unit - unit)->second * (1 - P);
-      if (is_debug) {
-        for (auto [k, v] : mp) cout << k << ':' << v << endl;
-        cout << j * unit << ' ' << mp.lower_bound(j * unit - unit)->first << endl;
-        cout << j * unit << ' ' << mp.lower_bound(-1000000000000000000LL)->first << endl;
+      for (int k = 0; k <= j; k++) {
+        double p1 = (--mp.upper_bound(min(OK_VALUE, j * unit + k * unit)))->second * P;
+        double p2 = (--mp.upper_bound(max(0, j * unit - k * unit)))->second * (1 - P);
+        new_mp[j * unit] = max(new_mp[j * unit], p1 + p2);
       }
-      new_mp[j * unit] = p1 + p2;
     }
     mp = new_mp;
   }
-  if (is_debug) {
-    for (auto [k, v] : mp) cout << k << ' ' << v << endl;
-  }
-  cout << mp.lower_bound(X)->second << endl;
+
+  cout << setprecision(10) << (--mp.upper_bound(X))->second << endl;
 }
