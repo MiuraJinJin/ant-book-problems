2c8ca2eb64f660e3f7e3e4f1873854b386e0109d
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-09-17 23:44:48 +0900

    P123 Millionaire

--- p123.cpp
+++ p123.cpp
@@ -3,49 +3,43 @@
 
 using namespace std;
 using namespace atcoder;
+typedef long long ll;
 bool is_debug;
 
-int dp[102][102];
 
-// P.123 Brive the Prisoners
 int main(int argc, char* argv[]) {
   is_debug = string(argv[0]) == "./test.out";
 
-  int P, Q;
-  cin >> P >> Q;
-  vector<int> A{ 0 };
-  for (int i = 0; i < Q; i++) {
-    int a;
-    cin >> a;
-    A.push_back(a);
-  }
-  A.push_back(P + 1);
+  int M;
+  cin >> M;
+  double P;
+  cin >> P;
+  int X;
+  cin >> X;
 
-  // Aは囚人のいるポジションであるが両端をそれぞれA[0], A[Q+1]とする
-  // dp[i][i+w]をi番目からi+w番目の囚人のいる空間(i,i+wそのものは含まない)の囚人を
-  // 解放するのに必要な金貨の最低枚数とする
-  // dp[i][i+1]は0となり、dp[i][i+w]=min(dp[i][k]+dp[k][i+w])+(A[i + w] - A[i] - 2)で漸化式を作れる
-  // A[i + w] - A[i] - 2は今回払う必要のある金貨の枚数
-  fill((int*) dp, (int*) (dp + 102), 1e9);
-  for (int i = 0; i <= Q; i++) dp[i][i + 1] = 0;
-  for (int w = 2; w <= Q + 1; w++) {
-    for (int i = 0; i <= Q + 1 - w; i++) {
-      for (int j = 1; j < w; j++) {
-        int k = i + j;
-        dp[i][i + w] = min(dp[i][i + w], (dp[i][k] + dp[k][i + w]) + (A[i + w] - A[i] - 2));
+  map<ll, double> mp;
+  const int OK_VALUE = 1000000;
+  mp[-1e18] = 0.0;
+  mp[OK_VALUE] = 1.0;
+  for (int i = 1; i <= M; i++) {
+    map<ll, double> new_mp;
+    new_mp[-1e18] = 0.0;
+    new_mp[OK_VALUE] = 1.0;
+    int unit = OK_VALUE / (1 << i);
+    for (int j = 1; j < 1 << i; j++) {
+      double p1 = mp.lower_bound(j * unit + unit)->second * P;
+      double p2 = mp.lower_bound(j * unit - unit)->second * (1 - P);
+      if (is_debug) {
+        for (auto [k, v] : mp) cout << k << ':' << v << endl;
+        cout << j * unit << ' ' << mp.lower_bound(j * unit - unit)->first << endl;
+        cout << j * unit << ' ' << mp.lower_bound(-1000000000000000000LL)->first << endl;
       }
+      new_mp[j * unit] = p1 + p2;
     }
-    if (is_debug) {
-      for (int i = 0; i <= Q + 1; i++) {
-        for (int j = 0; j <= Q + 1; j++) {
-          cout << dp[i][j] << ' ';
-        }
-        cout << endl;
-      }
-      cout << endl;
-    }
+    mp = new_mp;
   }
-
-  int ans = dp[0][Q + 1];
-  cout << ans << endl;
+  if (is_debug) {
+    for (auto [k, v] : mp) cout << k << ' ' << v << endl;
+  }
+  cout << mp.lower_bound(X)->second << endl;
 }
