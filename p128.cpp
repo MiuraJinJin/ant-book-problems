d9631d256e4ec513d3360796d58bd8751fa81aaa
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-09-19 11:43:20 +0900

    P.128 lower_bound

---
+++ p128.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+bool is_debug;
+
+int lower_bound(vector<int> A, int X) {
+
+  int ng = -1, ok = A.size();
+  while (ok - ng > 1) {
+    int mid = (ok + ng) / 2;
+    if (A[mid] >= X) ok = mid;
+    else ng = mid;
+  }
+  return ok;
+}
+
+int main(int argc, char* argv[]) {
+  is_debug = string(argv[0]) == "./test.out";
+
+  int N;
+  cin >> N;
+  vector<int> A;
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    A.push_back(a);
+  }
+  A.push_back(2e9);
+  int K;
+  cin >> K;
+
+  cout << lower_bound(A, K)<< endl;
+}
