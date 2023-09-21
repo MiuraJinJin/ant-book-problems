34053ccbaa7a5085db958332c1074a06f2e70f80
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-09-21 23:45:25 +0900

    P.141 Fliptile

---
+++ p141.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+bool is_debug;
+
+const pair<int, int> scope[5] = {
+  { -1, 0 },
+  { 0, -1 },
+  { 0, 0 },
+  { 0, 1 },
+  { 1, 0 },
+};
+
+int main(int argc, char* argv[]) {
+  is_debug = string(argv[0]) == "./test.out";
+
+  int M, N;
+  cin >> M >> N;
+  
+}
