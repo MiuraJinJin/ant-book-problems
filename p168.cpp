ab8cb704bd91f0cf6593b60984f6a73efbb236b7
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-10-01 23:38:56 +0900

    P.168 K-th Nmber 途中

--- p168.cpp
+++ p168.cpp
@@ -29,15 +29,40 @@ int main(int argc, char* argv[]) {
   vector<vector<ll>> sp_sorted;
   for (vector<ll> s : sp) {
     vector<ll> s_sorted(s);
+    sort(s_sorted.begin(), s_sorted.end());
     sp_sorted.push_back(s_sorted);
   }
+
+  vector<ll> sorted_A(A);
+  sort(sorted_A.begin(), sorted_A.end());
 
   for (int i = 0; i < M; i++) {
     ll a, b, k;
     cin >> a >> b >> k;
-    ll top = N - 1, bottom = -1;
-    while (top - bottom > 1) {
-      
+    ll ok = N - 1, ng = -1;
+    while (ok - ng > 1) {
+      ll mid = (ok + ng) / 2;
+      ll sp_start = -1, sp_end = -1;
+      for (int i = 0; i * n < N; i++) {
+        if (sp_start == -1 && i * n >= a) sp_start = i;
+        if (sp_end == -1 && i * n >= b) sp_end = i;
+      }
+      int wk = 0;
+      for (int i = a; i < sp_start * n; i++) if (sorted_A[mid] >= A[i]) wk++;
+      for (int i = (sp_end - 1) * n; i < b; i++) if (sorted_A[mid] >= A[i]) wk++;
+
+      for (int i = sp_start; i < sp_end; i++) {
+        wk += upper_bound(sp_sorted[i].begin(), sp_sorted[i].end(), sorted_A[mid]) - sp_sorted[i].begin();
+        if (is_debug) {
+          cout << upper_bound(sp_sorted[i].begin(), sp_sorted[i].end(), sorted_A[mid]) - sp_sorted[i].begin() << endl;
+        }
+      }
+      if (is_debug) {
+        cout << wk << ' ' << ok << ' ' << ng << endl;
+      }
+      if (wk > k) ng = mid;
+      else ok = mid;
     }
+    cout << ok << endl;
   }
 }
