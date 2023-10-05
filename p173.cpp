f2279808eb77322b3b9a738431918063f8244b64
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-10-05 23:41:58 +0900

    P.173 巡回セールスマン問題

--- p173.cpp
+++ p173.cpp
@@ -6,9 +6,21 @@ using namespace atcoder;
 typedef long long ll;
 bool is_debug;
 
-vector<pair<int, int>> D[15];
-// dp[i][bit], i: 現在位置, bit: 今まで通ってきたノード, value: コストの最小値
+// dp[i][bit], i: 現在の頂点, bit: 経路, value: そこまでの最小コスト
 int dp[15][1 << 15];
+vector<pair<int, int>> D[15];
+
+void recur(int i, int bit) {
+  for (auto [j, d] : D[i]) {
+    if (bit & (1 << j) == 1) continue;
+    int next_bit = bit | (1 << j);
+    if (dp[j][next_bit] > dp[i][bit] + d) {
+      dp[j][next_bit] = dp[i][bit] + d;
+      recur(j, next_bit);
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   is_debug = string(argv[0]) == "./test.out";
 
@@ -21,25 +33,6 @@ int main(int argc, char* argv[]) {
   }
   fill((int*) dp, (int*) (dp + 15), 1e9);
   dp[0][0] = 0;
-  deque<int> que({0});
-  while (!que.empty()) {
-    int i = que.front();
-    que.pop_front();
-    for (int bit = 0; bit < (1 << N); bit++) {
-      if (dp[i][bit] == 1e9) continue;
-      for (auto [j, d] : D[i]) {
-        if (dp[j][bit | 1 << j] < dp[i][bit] + d) {
-          dp[j][bit | 1 << j] = dp[i][bit] + d;
-          que.push_back(j);
-        }
-      }
-    }
-  }
-  int ans = 1e9;
-  for (int i = 0; i < N; i++) {
-    for (auto [j, d] : D[i]) {
-      if (j == 0) ans = min(ans, dp[i][(1 << N) - 2]);
-    }
-  }
-  cout << ans << endl;
+  recur(0, 0);
+  cout << dp[0][(1 << N) - 1] << endl;
 }
