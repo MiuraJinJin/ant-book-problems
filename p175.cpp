ac24e88a760c66a78accb06d40848a66c349810e
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-10-06 22:08:41 +0900

    P.175 Traveling by Stagecoach(途中

---
+++ p175.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+bool is_debug;
+
+int main(int argc, char* argv[]) {
+  is_debug = string(argv[0]) == "./test.out";
+
+  int N, M;
+  cin >> N >> M;
+}
