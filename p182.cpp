172ecefd0b6f8160d992fb7980f51edb345e680f
MiuraJinJin <119563516+MiuraJinJin@users.noreply.github.com>
2023-10-15 22:17:36 +0900

    P.182 Blocks(途中)

--- p182.cpp
+++ p182.cpp
@@ -18,23 +18,4 @@ int main(int argc, char* argv[]) {
     num.insert(N);
     N >>= 1;
   }
-  map<ll, mint> memo_even;
-  map<ll, mint> memo_odd;
-  for (ll n : num) {
-    if (n == 1) {
-      memo_even[n] = 2;
-      memo_odd[n] = 0;
-      continue;
-    }
-    if (n == 2) {
-      memo_even[n] = 2;
-      memo_odd[n] = 2;
-      continue;
-    }
-    if (num.contains(n * 2)) {
-      memo_even[2 * n] = memo_even[n] * memo_even[n] + memo_odd[n] * memo_odd[n];
-    }
-    else {
-    }
-  }
 }
